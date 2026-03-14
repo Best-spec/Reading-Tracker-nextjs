1f3e13bc0d42900c582149abe43ef56bdafd8faf
Best-spec <karami52468@gmail.com>
2026-03-15 05:35:36 +0700

    feat: implement addBook function in book management controller

--- src/server/controllers/bookManagement.ts
+++ src/server/controllers/bookManagement.ts
@@ -1,1 +1,5 @@
 import { Request, response } from "express";
+
+export const addBook = async (req: Request, res: Response) => {
+    try {
+        const { title, author, publishedDate } = req.body;
